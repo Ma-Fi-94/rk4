169cf7d415055cf0fa8622a35e3f8e34dd1a2417
Ma-Fi-94 <33027321+Ma-Fi-94@users.noreply.github.com>
2022-10-10 21:23:48 +0200

    Some refactoring

--- rk4.c
+++ rk4.c
@@ -9,10 +9,19 @@ double** solve(void (*f)(double, double*, double*),
            double tmax,
            int d) {
     
-    // Performance hack
-    long dthalf = dt/2;
+    // Init current system state with initial condition
+    double t = 0;
+    double* y = (double*) malloc(d * sizeof(double));
+    memcpy(y, y0, d*sizeof(double));
     
-    // RK coefficients
+    // Results to be stored here
+    long imax = (long) (tmax/dt);
+    double** res = (double**) malloc((1+d) * sizeof(double*));
+    for (int i = 0; i < 1+d; i++) {
+        res[i] = (double*) malloc(imax * sizeof(double));
+    }
+           
+    // For storing the RK coefficients during iteration
     double* k1 = (double*) malloc(d * sizeof(double));
     double* k2 = (double*) malloc(d * sizeof(double));
     double* k3 = (double*) malloc(d * sizeof(double));
@@ -21,49 +30,39 @@ double** solve(void (*f)(double, double*, double*),
     // For calculating y for the intermediate steps k2,k3,k4
     double* y_tmp = (double*) malloc(d * sizeof(double));
     
-    // Init current system state with initial condition
-    double t = 0;
-    double* y = (double*) malloc(d * sizeof(double));
-    memcpy(y, y0, d*sizeof(double));
+    // Performance hack for avoiding potential recalculation
+    long dthalf = dt/2;
     
-    // Results
-    long imax = (long) (tmax/dt);
-    double** res = (double**) malloc((1+d) * sizeof(double*));
-    for (int i = 0; i < 1+d; i++) {
-        res[i] = (double*) malloc(imax * sizeof(double));
-    }
-    
-    // Simulation loop
+    // Main integration loop
     for (long i = 0; i < imax; i++) {
         // Save current state
         res[0][i] = t;
         for (int j = 0; j < d; j++) {
             res[j+1][i] = y[j];
         }
-        //printf("%f, %f, %f, %f \n", t, y[0], y[1], y[2]);
         
-        // k1 equals the slope f(t, y)
+        // k1 = f(t, y)
         f(t, y, k1);
         
-        // k2 is the slope f(t+dt/2, y+dt/2*k1)
+        // k2 = f(t+dt/2, y+dt/2*k1)
         for (int j = 0; j < d; j++) {
             y_tmp[j] = y[j] + dthalf*k1[j];
         }
         f(t+dthalf, y_tmp, k2);
         
-        // k3 is the slope f(t+dt/2, y+dt/2*k2)        
+        // k3 = f(t+dt/2, y+dt/2*k2)        
         for (int j = 0; j < d; j++) {
             y_tmp[j] = y[j] + dthalf*k2[j];
         }
         f(t+dthalf, y_tmp, k3);
         
-        // k4 is the slope f(t+dt, y+dt*k3)
+        // k4 = f(t+dt, y+dt*k3)
         for (int j = 0; j < d; j++) {
             y_tmp[j] = y[j] + dt*k3[j];
         }       
         f(t+dt, y_tmp, k4);
         
-        // Recursion step y := y+dt*(1/6)*(k1+2k2+2k3+k4), t := t+dt
+        // Integration step y := y+dt*(1/6)*(k1+2k2+2k3+k4), t := t+dt
         for (int j = 0; j < d; j++) {
             y[j] = y[j] + dt*(1.0/6.0)*(k1[j]+k2[j]+k3[j]+k4[j]);
         }
@@ -82,11 +81,13 @@ void dealloc(double** ptr, int d) {
     free (ptr);
 }
 
+/////////////////////////////////////////////////////////////////////////////////
+// All the stuff below this block is just for running the program on its own.
+// It can be safely ignored if the code is to be used as a shared object.
+/////////////////////////////////////////////////////////////////////////////////
+
 void lorenz(double t, double* y, double* dydt) {
-    /*
-     * The Lorenz system as an example function to solve.
-     */
-    
+    // The Lorenz system as an example function to solve.   
     dydt[0] = 10.0*(y[1]-y[0]);
     dydt[1] = y[0]*(28.0-y[2]) - y[1];
     dydt[2] = y[0]*y[1] - (8.0/3.0)*y[2];
