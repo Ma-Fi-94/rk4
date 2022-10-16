03832671eca64c6c78945e89095e0012cc061680
Matthias M. Fischer <33027321+Ma-Fi-94@users.noreply.github.com>
2022-10-16 15:48:44 +0200

    Add files via upload

--- call_so_manually.py
+++ call_so_manually.py
@@ -5,8 +5,6 @@ from ctypes import *
 libname = "./rk4.so"
 c_lib = CDLL(libname)
 
-############################################
-
 # Want to call:
 # double** solve(void (*f)(double, double*, double*), double* y0, double dt, double tmax, int d)
 solve = c_lib.solve
@@ -16,6 +14,47 @@ solve.argtypes = [
     POINTER(c_double), c_double, c_double, c_int
 ]
 
+############################################
+
+# A simple 1d ODE which we can analytically solve to check the numerics
+
+# We want to pass this Python function to the solver, thus we need to properly annotate it
+@CFUNCTYPE(None, c_double, POINTER(c_double), POINTER(c_double))
+def expdecay(t, y, dydt):
+    dydt[0] = -0.1 * y[0]
+
+
+# The params
+y0 = (c_double * 1)(100.0)
+y0 = cast(y0, POINTER(c_double))
+dt = c_double(1e-6)
+tmax = c_double(1.0)
+d = c_int(1)
+
+# Calling the function, getting a double** as return
+ret = solve(expdecay, y0, dt, tmax, d)
+
+# Print last value of numerical solution
+y_final_num = ret[1][int(1.0/1e-6)-1]
+print("y(1.0) numerical: ", y_final_num)
+
+y_final_anal = 100*(2.71)**(1.0*(-0.1))
+print("y(1.0) analytical: ", y_final_anal)
+
+print("Difference num. vs. anal. solution:", (y_final_num-y_final_anal)/y_final_anal)
+# TODO: There seems to be some problem in the solver itself.
+
+# dealloc in C, to avoid leak
+dealloc = c_lib.dealloc
+dealloc.argtypes = [POINTER(POINTER(c_double)), c_int]
+dealloc.restype = None
+dealloc(ret, 3)
+
+
+
+############################################
+
+# A more complex example: the 3d chaotic Lorenz system
 
 # We want to pass this Python function to the solver, thus we need to properly annotate it
 @CFUNCTYPE(None, c_double, POINTER(c_double), POINTER(c_double))
@@ -28,7 +67,6 @@ def lorenz(t, y, dydt):
 # The params
 y0 = (c_double * 3)(1, 2, 3)
 y0 = cast(y0, POINTER(c_double))
-
 dt = c_double(0.0001)
 tmax = c_double(10.0)
 d = c_int(3)
@@ -45,3 +83,4 @@ dealloc = c_lib.dealloc
 dealloc.argtypes = [POINTER(POINTER(c_double)), c_int]
 dealloc.restype = None
 dealloc(ret, 3)
+
